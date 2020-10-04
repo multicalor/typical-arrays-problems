c1c1c4868c8416dec897876ae0631b568203dc06
multicalor <d.tretiuk@gmil.com>
2020-10-04 08:47:13 +0300

    init: start task Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+exports.min = function min(array) {
+  let result = 0;
+  if (!array) {
+      return 0;
+  }
 
-exports.min = function min (array) {
-  return 0;
-}
+  array.forEach((element) => {
+      result > element ? (result = element) : result;
+  });
+  return result;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  let result = 0;
+  if (!array) {
+      return 0;
+  }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+  array.forEach((element) => {
+      result < element ? (result = element) : result;
+  });
+  return result;
+};
+
+exports.avg = function avg(array) {
+  if (!array || array.length === 0) {
+      return 0;
+  }
+  let result = array.reduce((sum, current) => sum + current, 0);
+
+  return result/array.length;
+};
